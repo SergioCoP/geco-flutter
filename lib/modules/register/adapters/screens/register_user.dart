7bff62c2a6ea6a1d4138775bb4bb585adc1b5d8b
20203tn125 <20203tn125@utez.edu.mx>
2023-11-01 17:00:51 -0600

    [ADD] Consumo de servicio de registro

--- lib/modules/register/adapters/screens/register_user.dart
+++ lib/modules/register/adapters/screens/register_user.dart
@@ -2,7 +2,14 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:geco_mobile/kernel/theme/color_app.dart';
 
-class RegisterUser extends StatelessWidget {
+class RegisterUser extends StatefulWidget {
+  const RegisterUser({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _RegisterUser();
+}
+
+class _RegisterUser extends State<RegisterUser> {
   final _formKey = GlobalKey<FormState>();
 
   bool _isButtonDisabled = true;
@@ -15,10 +22,9 @@ class RegisterUser extends StatelessWidget {
   final TextEditingController _contrasena = TextEditingController(text: '');
   final TextEditingController _confcontrasena = TextEditingController(text: '');
   final dio = Dio();
-  RegisterUser({super.key});
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
         body: Column(
       children: [
@@ -40,127 +46,164 @@ class RegisterUser extends StatelessWidget {
           ),
         ),
         Expanded(
-          child: Container(
-            alignment: Alignment.center,
-            color: ColorsApp.primaryColor,
-            child: Card(
-              margin: const EdgeInsets.all(15),
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15)),
-              child: Column(children: [
-                Form(
-                  key: _formKey,
-                  onChanged: () {},
-                  child: Column(
-                    children: <Container>[
-                      Container(
-                        margin: const EdgeInsets.all(20),
-                        child: Column(children: [
-                          const Text(
-                            'Registro',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 22, fontWeight: FontWeight.bold),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(top: 12),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                  hintText: "Nombre",
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(15),
-                                  )),
-                              keyboardType: TextInputType.text,
-                              controller: _nombre,
+          child: SingleChildScrollView(
+            child: Container(
+              alignment: Alignment.center,
+              color: ColorsApp.primaryColor,
+              child: Card(
+                margin: const EdgeInsets.all(15),
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                child: Column(children: [
+                  Form(
+                    key: _formKey,
+                    onChanged: () {
+                      setState(() {
+                        _isButtonDisabled = !_formKey.currentState!.validate();
+                      });
+                    },
+                    child: Column(
+                      children: <Container>[
+                        Container(
+                          margin: const EdgeInsets.all(20),
+                          child: Column(children: [
+                            const Text(
+                              'Registro',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 22, fontWeight: FontWeight.bold),
                             ),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(top: 12),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                  hintText: "Apellido Paterno",
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(15),
-                                  )),
-                              keyboardType: TextInputType.text,
-                              controller: _aPaterno,
+                            Container(
+                              margin: const EdgeInsets.only(top: 12),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    hintText: "Nombre",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    )),
+                                keyboardType: TextInputType.text,
+                                controller: _nombre,
+                              ),
                             ),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(top: 12),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                  hintText: "Apellido Materno",
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(15),
-                                  )),
-                              keyboardType: TextInputType.text,
-                              controller: _aMaterno,
+                            Container(
+                              margin: const EdgeInsets.only(top: 12),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    hintText: "Apellido Paterno",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    )),
+                                keyboardType: TextInputType.text,
+                                controller: _aPaterno,
+                              ),
                             ),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(top: 10),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                  hintText: "Correo",
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(15),
-                                  )),
-                              keyboardType: TextInputType.emailAddress,
-                              controller: _correo,
+                            Container(
+                              margin: const EdgeInsets.only(top: 12),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    hintText: "Apellido Materno",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    )),
+                                keyboardType: TextInputType.text,
+                                controller: _aMaterno,
+                              ),
                             ),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(top: 12),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                  hintText: "Contraseña",
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(15),
-                                  )),
-                              keyboardType: TextInputType.text,
-                              controller: _contrasena,
+                            Container(
+                              margin: const EdgeInsets.only(top: 10),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    hintText: "Correo",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    )),
+                                keyboardType: TextInputType.emailAddress,
+                                controller: _correo,
+                              ),
                             ),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(top: 12),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                  hintText: "Confirmar contraseña",
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(15),
-                                  )),
-                              keyboardType: TextInputType.text,
-                              controller: _confcontrasena,
+                            Container(
+                              margin: const EdgeInsets.only(top: 12),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    hintText: "Contraseña",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    )),
+                                keyboardType: TextInputType.text,
+                                controller: _contrasena,
+                              ),
                             ),
-                          ),
-                          Container(
-                              margin: const EdgeInsets.all(10),
-                              child: Column(
-                                children: [
-                                  Container(
-                                    margin: const EdgeInsets.only(top: 15),
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(14)),
-                                          minimumSize: const Size(400, 60),
-                                          backgroundColor:
-                                              ColorsApp.secondaryColor),
-                                      onPressed: () {},
-                                      child: const Text('Registrar'),
-                                    ),
-                                  )
-                                ],
-                              )),
-                        ]),
-                      )
-                    ],
-                  ),
-                )
-              ]),
+                            Container(
+                              margin: const EdgeInsets.only(top: 12),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    hintText: "Confirmar contraseña",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    )),
+                                keyboardType: TextInputType.text,
+                                controller: _confcontrasena,
+                              ),
+                            ),
+                            Container(
+                                margin: const EdgeInsets.all(10),
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      margin: const EdgeInsets.only(top: 15),
+                                      child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(14)),
+                                            minimumSize: const Size(400, 60),
+                                            backgroundColor:
+                                                ColorsApp.secondaryColor),
+                                        onPressed: _isButtonDisabled
+                                            ? null
+                                            : () async {
+                                                Response response;
+                                                try {
+                                                  response = await dio.request(
+                                                      'http://192.168.32.1:8080/registerUser',
+                                                      data: {
+                                                        'email': _correo.text,
+                                                        'password':
+                                                            _contrasena.text,
+                                                        'idPerson': {
+                                                          'name': _nombre.text,
+                                                          'surname':
+                                                              _aPaterno.text,
+                                                          'lastname':
+                                                              _aMaterno.text
+                                                        },
+                                                      },
+                                                      options: Options(
+                                                          method: 'POST'));
+
+                                                  if (response.data ==
+                                                      'register') {
+                                                    print('Registrado');
+                                                  } else {
+                                                    print('Error al registrar');
+                                                  }
+                                                } catch (e) {
+                                                  print(e);
+                                                }
+                                              },
+                                        child: const Text('Registrar'),
+                                      ),
+                                    )
+                                  ],
+                                )),
+                          ]),
+                        )
+                      ],
+                    ),
+                  )
+                ]),
+              ),
             ),
           ),
         )
